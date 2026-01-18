6b7408bff3b6c74b40b3454b72adb23f5ccb0732
Mentalily <3339672417@qq.com>
2026-01-18 21:58:21 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="phone-screen">
+    <div class="status-bar">12:00 100%</div>
+
+    <RouterView />
+
+    <div class="home-bar"></div>
+  </div>
+</template>
+
+<style scoped></style>
