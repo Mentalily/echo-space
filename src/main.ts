2e4564cb6113fda06bfe32ff13e7209c7645f13e
Mentalily <3339672417@qq.com>
2026-01-21 17:56:14 +0800

    基础设计框架+基础路由配置

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'    // creatApp: 创建应用
 import { createPinia } from 'pinia'
 
-import App from './App.vue'        // 组件
+import App from './App.vue'        // 根组件
 import router from './router'
 
 const app = createApp(App)
@@ -11,4 +11,10 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.mount('#app') // 挂载到 <div id='app'></div> 上
+
+/*
+* 应用实例必须在调用了 .mount() 方法后才会渲染出来
+* 该方法接收一个“容器”参数，可以是一个实际的 DOM 元素或是一个 CSS 选择器字符串
+* 应用根组件的内容将会被渲染在容器元素里面。容器元素自己将不会被视为应用的一部分。
+* */
---
+++ src/router/index.ts
@@ -0,0 +1,78 @@
+import {createRouter, createWebHistory} from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
+import AnniversaryView from "@/views/AnniversaryView.vue";
+import ChatView from "@/views/ChatView.vue";
+import ContactView from "@/views/ContactView.vue";
+import GalleryView from "@/views/GalleryView.vue";
+import HomeView from "@/views/HomeView.vue";
+import MemoView from "@/views/MemoView.vue";
+import MusicView from "@/views/MusicView.vue";
+import PeriodView from "@/views/PeriodView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import TomaView from "@/views/TomaView.vue";
+import TypeWriterView from "@/views/TypeWriterView.vue";
+import WalletView from "@/views/WalletView.vue";
+import WishlistView from "@/views/WishlistView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        component: HomeView,
+    },
+    {
+        path: '/chat',
+        component: ChatView,
+    },
+    {
+        path: '/date',
+        component: AnniversaryView,
+    },
+    {
+        path: '/gallery',
+        component: GalleryView,
+    },
+    {
+        path: '/set',
+        component: SettingsView,
+    },
+    {
+        path: '/music',
+        component: MusicView,
+    },
+    {
+        path:'/memo',
+        component: MemoView,
+    },
+    {
+        path:'/toma',
+        component: TomaView,
+    },
+    {
+        path:'/period',
+        component: PeriodView,
+    },
+    {
+        path:'/writer',
+        component: TypeWriterView,
+    },
+    {
+        path:'/wish',
+        component: WishlistView,
+    },
+    {
+        path:'/wallet',
+        component: WalletView,
+    },
+    {
+        path:'/contact',
+        component: ContactView,
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
